480ee80a8f77d2c960b01daad60f951dba78758b
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2020-02-09 00:13:22 +0530

    chapter1

--- src/com/ht/algorithms/Main.java
+++ src/com/ht/algorithms/Main.java
@@ -1,8 +1,37 @@
 package com.ht.algorithms;
 
+import com.ht.algorithms.chapter1.Factorial;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        printTable(5);
+    }
+
+    static int[] sumPreviousIndex(int[] arr) {
+        return sumPreviousIndex(arr, 1);
+    }
+    static int[] sumPreviousIndex(int[] arr, int index) {
+        if(arr.length <= index || index < 0)  return arr;
+        arr[index] += arr[index-1];
+        sumPreviousIndex(arr, ++index);
+        return arr;
+    }
+
+    static void toworOfHanoi(char S, char D, char E, int N) {
+        if(N <= 0) return;
+        toworOfHanoi(S, E, D, N-1);
+        System.out.printf("Move %d from %c to %c \n", N, S, D);
+        toworOfHanoi(E, D, S, N-1);
+    }
+
+    static void  printTable(int n) {
+        printTable(n, 10);
+    }
+    static void printTable(int n, int i) {
+        if(i > 0) {
+            printTable(n, i-1);
+            System.out.printf("%d * %d = %d \n", n, i, n*i);
+        }
     }
 }
