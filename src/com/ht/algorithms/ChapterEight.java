f44d1f96168a300b4156f5c8ee48f73a819c7fde
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2020-02-15 15:00:41 +0530

    chapter 8

--- src/com/ht/algorithms/ChapterEight.java
+++ src/com/ht/algorithms/ChapterEight.java
@@ -62,4 +62,106 @@ public class ChapterEight {
         }
         return cost[cost.length-1][cost[0].length-1];
     }
+
+    /**
+     * Given an empty plot of size 2 x n . We want to place tiles such that the entire plot is covered. Each tile is of size
+     * 2 x 1 and can be placed either horizontally or vertically. If n is 5 , then one way to cover the plot
+     * Write a function that accept n as input and return the total number of ways in which we can place the tiles (without breaking any tile).
+     * @param n
+     * @return
+     */
+    public static int countWays(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        return countWays(n-1) + countWays(n-2);
+    }
+
+    public static int countWaysFor3Row(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 0;
+        if(n == 2) return 3;
+
+        return countWaysFor3Row(n-2) * 3;
+    }
+
+    /**
+     * Consider a game where a player can score 3, 5 or 10 points in one move. Given a total score N ,
+     * find the total number of unique ways to reach a score of N .
+     * @param n
+     * @return
+     */
+    public static int waysToScore(int n) {
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+        return waysToScore(n-3) + waysToScore(n-5) + waysToScore(n-10);
+    }
+
+    public static int waysToScoreDP(int n) {
+        int[] count = new int[n+1];
+        count[0] = 1;
+        for (int i = 1; i < count.length; i++) {
+            if(i-3 >= 0) count[i] += count[i-3];
+            if(i-5 >= 0) count[i] += count[i-5];
+            if(i-10 >= 0) count[i] += count[i-10];
+        }
+        return count[count.length-1];
+    }
+
+    /**
+     * What is the total number of ways to reach a particular score if (10, 3) and (3, 10) same
+     * @param n
+     * @return
+     */
+    public static int waysToScoreDPUnique(int n) {
+        int[] count = new int[n+1];
+
+        count[0] =  1;
+        for (int i = 3; i < count.length; i++) {
+            count[i] += count[i-3];
+        }
+        for (int i = 5; i < count.length; i++) {
+            count[i] += count[i-5];
+        }
+
+        for (int i = 10; i < count.length; i++) {
+            count[i] += count[i-10];
+        }
+
+        return count[count.length-1];
+    }
+
+
+    /**
+     * Given an array of integers, write a function that
+     * returns the maximum sum of sub array, such that elements are contiguous.
+     */
+
+    public static int maxSubArraySum(int[] a) {
+        int max = Integer.MIN_VALUE;
+        int tempMax;
+        for (int i = 0; i < a.length; i++) {
+            tempMax = a[i];
+            for (int j = i+1; j < a.length; j++) {
+                tempMax += a[j];
+                if(tempMax > max) max = tempMax;
+            }
+        }
+        return max;
+    }
+
+    public static int maxSubArraySumDP(int[] a) {
+        int maxSum = 0;
+        int maxSumSoFar = 0;
+        for (int i = 0; i < a.length; i++) {
+            maxSumSoFar += a[i];
+            if(maxSumSoFar < 0) maxSumSoFar = 0;
+            if(maxSum < maxSumSoFar) maxSum = maxSumSoFar;
+        }
+
+        return maxSum;
+    }
+
+
 }
--- src/com/ht/algorithms/Main.java
+++ src/com/ht/algorithms/Main.java
@@ -5,14 +5,14 @@ import netscape.security.UserTarget;
 public class Main {
 
     public static void main(String[] args) {
-          int[][] cost = {
-                  {1,3,5,8},
-                  {4,2,1,7},
-                  {4,3,2,3}
-          };
-          System.out.println(ChapterEight.minPathCost3Way(cost));
+//        int[][] cost = {
+//                {1,3,5,8},
+//                {4,2,1,7},
+//                {4,3,2,3}
+//        };
+//        System.out.println(ChapterEight.minPathCost3Way(cost));
 //        System.out.println(ChapterSIX.maxSubStringLengthDP("142124"));
-//        0123456
+
 //        int[][] arr = new int[][] {
 //                {0, 10, 75, 94},
 //                {-1, 0, 35, 50},
@@ -20,7 +20,11 @@ public class Main {
 //                {-1, -1, -1, 0}
 //        };
 //        System.out.println(ChapterSIX.costBetweenStation(arr));
+//        System.out.println(ChapterEight.waysToScoreDPUnique(13));
+            int[] a = new int[] {-2, -3, 4, -1, -2, 1, 5, -3};
+            System.out.println(ChapterEight.maxSubArraySumDP(a));
     }
+
 
 
 
